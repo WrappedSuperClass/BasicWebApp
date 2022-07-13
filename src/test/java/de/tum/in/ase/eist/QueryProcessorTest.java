2980dc2812edbb294018f16fdf2ec3950ee31183
Lukas <ge64diw@mytum.de>
2022-07-13 17:33:22 +0200

    sd

--- src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
+++ src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
@@ -32,7 +32,7 @@ class QueryProcessorTest {
 		@Test
 		public void isNotCaseSensitive2() throws Exception {
 
-		assertTrue(queryProcessor.process("name").contains("Freddi"));
+		assertTrue(queryProcessor.process("name").contains("freddi"));
 }
 
 
